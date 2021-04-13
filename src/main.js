5ee2a199271b5352e48917b488270165cbe03209
haozhixiong <13641040120@163.com>
2021-04-13 17:49:55 +0800

    提交模态框组件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Table from '@/components/Table'
 import SearchArea from '@/components/SearchArea'
 import Pagination from '@/components/Pagination'
 import Export from '@/components/Export'
-import EvsTablePage from '@/comments/EvsTablePage'
-import EvsSearchArea from '@/comments/EvsSearchArea'
-
-export { Table, SearchArea, Pagination, Export, EvsTablePage, EvsSearchArea }
+import EvsTablePage from '@/components/EvsTablePage'
+import EvsSearchArea from '@/components/EvsSearchArea'
+import EvsModel from '@/components/model'
+export { Table, SearchArea, Pagination, Export, EvsTablePage, EvsSearchArea,EvsModel }
