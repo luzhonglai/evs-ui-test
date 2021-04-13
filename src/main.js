7fb7039cdddfb0ba723066f1816071e1a16d98bd
jiangpeng <13120009613@163.com>
2021-04-13 18:06:22 +0800

    代码目录修改

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Table from '@/components/Table'
-import SearchArea from '@/components/SearchArea'
-import Pagination from '@/components/Pagination'
-import Export from '@/components/Export'
-import EvsTablePage from '@/components/EvsTablePage'
-import EvsSearchArea from '@/components/EvsSearchArea'
-import EvsModel from '@/components/model'
-export { Table, SearchArea, Pagination, Export, EvsTablePage, EvsSearchArea,EvsModel }
+/*
+ * @Descripttion:
+ * @Author: Jiang Peng
+ * @Date: 2021-04-13 17:50:03
+ * @LastEditors: Jiang Peng
+ * @LastEditTime: 2021-04-13 17:58:11
+ */
+import Table from "@/components/Table";
+import SearchArea from "@/components/SearchArea";
+import Pagination from "@/components/Pagination";
+import Export from "@/components/Export";
+import EvsTablePage from "@/components/EvsTablePage";
+import EvsSearchArea from "@/components/EvsSearchArea";
+import EvsModel from "@/components/EvsModel/model";
+export {
+  Table,
+  SearchArea,
+  Pagination,
+  Export,
+  EvsTablePage,
+  EvsSearchArea,
+  EvsModel,
+};
