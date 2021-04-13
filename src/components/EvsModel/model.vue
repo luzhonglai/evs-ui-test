8beb17d3fea09763763bb96e089b73667d39fdb3
haozhixiong <13641040120@163.com>
2021-04-13 15:44:21 +0800

    提交模态框组件

---
+++ src/components/EvsModel/model.vue
@@ -0,0 +1,176 @@
+<!--  -->
+<template>
+  <div class="model">
+    <el-button type="primary" plain @click="showModel">{{
+      actionBtnText
+    }}</el-button>
+    <el-dialog
+      :model-value="modelIsFlag"
+      :width="width"
+      v-bind="attrs"
+      :show-close="showCloseIcon"
+      :destroy-on-close="destrorOnClose"
+      @close="closeModel"
+    >
+      <template #title>
+        <div
+          class="model-title"
+          :style="[titleAlignActive, titleCssTextActive]"
+        >
+          {{ title }}
+        </div>
+      </template>
+      <template v-if="titleComponent">
+        <div class="vfor-title" v-for="item in titleComponent" :key="item">
+          <EVSTitle :itemTitle="item.title" :isClass="item.isClass">
+            <slot :name="item.slot"></slot>
+          </EVSTitle>
+        </div>
+      </template>
+      <slot></slot>
+      <slot name="footer"></slot>
+    </el-dialog>
+  </div>
+</template>
+
+<script lang='ts'>
+import EVSTitle from "./title.vue";
+import {
+  defineComponent,
+  reactive,
+  toRefs,
+  ref,
+  watch,
+  onBeforeMount,
+  onMounted,
+  computed,
+} from "vue";
+export default defineComponent({
+  name: "EVSeModel",
+  components: {
+    EVSTitle,
+  },
+  props: {
+    modelIsFlag: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: [String, Number],
+      default: "提示",
+    },
+    width: {
+      type: [String, Number],
+      default: () => {
+        return "50%";
+      },
+    },
+    actionBtnText: {
+      type: String,
+      default: "确认",
+    },
+    destrorOnClose: {
+      type: Boolean,
+      default: true,
+    },
+    titleAlign: {
+      type: String,
+      default: "left",
+    },
+    titleCssText: {
+      //  模态框标题 自定义颜色大小对齐方式
+      type: [Object, Array],
+      default: () => {
+        return {};
+      },
+    },
+    titleComponent: {
+      type: [Object, Array],
+      default: () => {
+        return [];
+      },
+    },
+  },
+  emits: ["handleComfirm", "handleShowModel", "handleCloseModel"],
+  setup(props: any, { emit, attrs }: any) {
+    const titleAlign = props.titleAlign; // 标题对齐方式
+    const titleCssTextActive = props.titleCssText; // 标题文字css 自定义
+    const titleAlignActive = computed(() => {
+      // title 文字 对齐方式
+      return "text-align:" + props.titleAlign;
+    });
+    const showCloseIcon = computed(() => {
+      // 按钮右对齐 ，隐藏关闭按钮
+      if (titleAlign == "right") {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    onBeforeMount(() => {});
+    onMounted(() => {});
+    const showModel = () => {
+      // 显示
+      emit("handleShowModel", true);
+    };
+    const closeModel = () => {
+      // 关闭
+      emit("handleCloseModel", false);
+    };
+    const handleAction = () => {
+      emit("handleComfirm");
+    };
+    return {
+      attrs,
+      showModel,
+      closeModel,
+      titleAlignActive,
+      showCloseIcon,
+      titleCssTextActive,
+      handleAction,
+    };
+  },
+});
+</script>
+<style scoped lang='less'>
+.model {
+  .model-title {
+    height: 56px;
+    text-align: left;
+    border-bottom: 1px solid #dcdcdc;
+  }
+  v-deep {
+    @buttonColor: #5587f0;
+    .el-button {
+      min-height: 36px !important;
+      line-height: 0 !important;
+      height: 36px !important;
+    }
+    .el-button--primary {
+      color: #fff !important;
+      border-radius: 2px;
+      background-color: @buttonColor !important;
+      border-color: @buttonColor !important;
+      &:hover {
+        background-color: @buttonColor !important;
+      }
+      .is-plain {
+        background: @buttonColor !important;
+        border-color: @buttonColor !important;
+        &:hover {
+          background-color: @buttonColor !important;
+        }
+        &:focus {
+          background-color: @buttonColor !important;
+        }
+      }
+    }
+    .el-button--default {
+      color: @buttonColor;
+      border-radius: 2px;
+      border: 1px solid;
+      border-color: @buttonColor;
+    }
+  }
+}
+</style>
---
+++ src/components/EvsModel/title.vue
@@ -0,0 +1,95 @@
+<!--  -->
+<template>
+  <div class="title">
+    <div class="title-text" :class="{ 'title_b': isClass }">
+      {{ itemTitle }}
+      <div v-if="isClass">
+        <i
+          class="el-icon-arrow-right item"
+          :class="{ 'is-active': show }"
+          style="cursor: pointer; margin-left: 20px"
+          @click="showBox"
+        ></i>
+      </div>
+    </div>
+  </div>
+  <div >
+    <el-collapse-transition>
+      <div v-show="show && isClass">
+        <slot></slot>
+      </div>
+    </el-collapse-transition>
+  </div>
+  <div v-if="!isClass">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang='ts'>
+import {
+  defineComponent,
+  ref,
+  onBeforeMount,
+  onMounted,
+} from 'vue'
+export default defineComponent({
+  name: 'EVSTitle',
+  props: {
+    itemTitle: {
+      type: String,
+      default: '',
+    },
+    isClass: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['showActive'],
+  setup(props: any, { emit, attrs }: any) {
+    const show = ref(true)
+    onBeforeMount(() => {
+    })
+    onMounted(() => {
+    })
+    const showBox = () => {
+      show.value = !show.value
+      emit('showActive', show.value)
+    }
+    return {
+      showBox,
+      show,
+    }
+  },
+})
+</script>
+<style scoped lang='less'>
+.title {
+  .title-text {
+    height: 40px;
+    background: rgba(85, 135, 240, 0.1);
+    font-size: 14px;
+    padding-left: 10px;
+    border-radius: 3px;
+    line-height: 40px;
+    margin: 0 0 10px;
+    color: #4a4a4a;
+    text-align: left;
+    border-left: 2px solid #5587f0;
+  }
+  .item {
+    transition: transform 0.3s;
+    font-weight: 300;
+  }
+  .title_b {
+    display: flex;
+    justify-content: space-between;
+    padding-right: 10px;
+  }
+  .is-active {
+    transform: rotate(90deg);
+  }
+  .transtion-ani {
+    transition: all 0.3s;
+  }
+}
+</style>
