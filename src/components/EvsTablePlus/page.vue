16c7641632207f3581a0a77ceccf1c2e6263e30a
haozhixiong <13641040120@163.com>
2021-05-10 14:35:01 +0800

    fix/page 组件样式更新

--- src/components/EvsTablePlus/page.vue
+++ src/components/EvsTablePlus/page.vue
@@ -1,67 +1,93 @@
 <!--  -->
 <template>
   <div>
-    <div class="pagination">
-      <div v-if="pageOtions.btnType">
-        <el-button
-          v-if="pageOtions.homePage ? pageOtions.homePage : false"
-          :type="pageOtions.btnType"
+    <div class="pagination" :style="block">
+      <div class="page-con">
+        <div
+          v-if="pageOtions.btnType"
+          :style="{ display: pageOtions.homePage ? 'block' : 'none' }"
           class="page-btn page-left"
-          size="mini"
-          @click="indexPage"
-          >首页</el-button
         >
-      </div>
-      <div v-else>
-        <el-button
-          v-if="pageOtions.homePage ? pageOtions.homePage : false"
+          <el-button
+            v-if="pageOtions.homePage ? pageOtions.homePage : false"
+            :type="pageOtions.btnType || 'primary'"
+            size="mini"
+            @click="indexPage"
+            >首页</el-button
+          >
+        </div>
+        <div
+          v-else
           class="page-btn page-left"
-          size="mini"
-          plain
-          @click="indexPage"
-          >首页</el-button
+          :style="{ display: pageOtions.homePage ? 'block' : 'none' }"
         >
-      </div>
-      <div v-if="pageOtions.btnType">
-        <el-button
-          v-if="pageOtions.homePage ? pageOtions.homePage : false"
-          :type="pageOtions.btnType"
+          <el-button
+            v-if="pageOtions.homePage ? pageOtions.homePage : false"
+            size="mini"
+            plain
+            @click="indexPage"
+            >首页</el-button
+          >
+        </div>
+        <div
+          v-if="pageOtions.btnType"
+          :style="{ display: pageOtions.homePage ? 'block' : 'none' }"
           class="page-btn page-right"
-          size="mini"
-          @click="lastPage"
-          >尾页</el-button
         >
-      </div>
-      <div v-else>
-        <el-button
-          v-if="pageOtions.homePage ? pageOtions.homePage : false"
+          <el-button
+            v-if="pageOtions.homePage ? pageOtions.homePage : false"
+            :type="pageOtions.btnType || 'primary'"
+            size="mini"
+            @click="lastPage"
+            >尾页</el-button
+          >
+        </div>
+        <div
+          v-else
           class="page-btn page-right"
-          size="mini"
-          plain
-          @click="lastPage"
-          >尾页</el-button
+          :style="{ display: pageOtions.homePage ? 'block' : 'none' }"
         >
+          <el-button
+            v-if="pageOtions.homePage ? pageOtions.homePage : false"
+            size="mini"
+            plain
+            @click="lastPage"
+            >尾页</el-button
+          >
+        </div>
+          <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :background="pageOtions.isColor"
+            :prev-text="pageOtions.prevText"
+            :next-text="pageOtions.nextText"
+            :current-page="pageOtions.page"
+            :page-sizes="pageOtions.sizes"
+            :page-size="pageOtions.size"
+            :total="pageOtions.total"
+            :layout="pageOtions.layout.join()"
+            :hide-on-single-page="
+              pageOtions.hidePage ? pageOtions.hidePage : false
+            "
+          >
+          </el-pagination>
       </div>
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :background="pageOtions.isColor"
-        :prev-text="pageOtions.prevText"
-        :next-text="pageOtions.nextText"
-        :current-page="pageOtions.page"
-        :page-sizes="pageOtions.sizes"
-        :page-size="pageOtions.size"
-        :total="pageOtions.total"
-        :layout="pageOtions.layout.join()"
-        :hide-on-single-page="pageOtions.hidePage ? pageOtions.hidePage : false"
-      >
-      </el-pagination>
+      <span class="page-num" v-if="pageOtions.pageCount">
+        第 {{ pageOtions.page }} 页 {{ pageOtions.size }} 条/页
+        <b>共 {{ pageOtions.total }} 条</b>
+      </span>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs, onBeforeMount, onMounted } from 'vue'
+import {
+  defineComponent,
+  reactive,
+  toRefs,
+  onBeforeMount,
+  onMounted,
+} from 'vue'
 interface Val {
   val: number | string
 }
@@ -75,6 +101,7 @@ export default defineComponent({
           page: 1,
           size: 10,
           total: 10,
+          pageCount: false,
           sizes: [10, 20, 30, 40, 50],
           layout: ['sizes', 'prev', 'pager', 'next', 'jumper'],
           isColor: true,
@@ -82,18 +109,21 @@ export default defineComponent({
           hidePage: false,
           prevText: '上一页',
           nextText: '下一页',
-          homePage: true,
+          homePage: false,
         }
       },
     },
   },
   emits: ['handelUpdataPage'],
   setup(props: any, { emit }: any) {
+    const block = props.pageOtions.homePage
+      ? 'display:flex'
+      : 'display:block;text-align:right'
     // 返回首页
     const indexPage = () => {
       console.log(props.pageOtions.page, 'children--table')
       emit('handelUpdataPage', {
-        value: 'index',
+        value: 'first',
         page: 1,
       })
     }
@@ -121,6 +151,7 @@ export default defineComponent({
       })
     }
     return {
+      block,
       indexPage,
       lastPage,
       handleSizeChange,
@@ -132,8 +163,22 @@ export default defineComponent({
 <style scoped lang="less">
 .pagination {
   position: relative;
-  width: 738px;
-  margin: 15px auto;
+  margin-top: 24px;
+  display: flex;
+  justify-content: flex-end;
+  .page-con {
+    position: relative;
+    padding-left: 60px;
+  }
+  .page-num {
+    padding-left: 50px;
+    line-height: 32px;
+    font-size: 14px;
+    b {
+      font-weight: normal;
+      padding-left: 20px;
+    }
+  }
   .page-btn {
     width: 60px;
     border-radius: 2px;
@@ -146,6 +191,11 @@ export default defineComponent({
     margin-top: 2px;
     display: inline-block;
     position: absolute;
+    // overflow: hidden;
+    ::v-deep(.el-button--primary) {
+      height: 28px !important;
+      min-height: 28px !important;
+    }
   }
 
   .page-left {
@@ -153,20 +203,37 @@ export default defineComponent({
   }
 
   .page-right {
-    right: 0;
+    right: 108px;
   }
   /deep/.el-pagination .el-select .el-input {
     width: 88px;
   }
+  ::v-deep {
+    .el-pagination.is-background .btn-next,
+    .el-pagination.is-background .btn-prev,
+    .el-pagination.is-background .el-pager li {
+      background: none !important;
+    }
+    .el-pagination.is-background .el-pager li:not(.disabled).active {
+      color: #5587f0;
+      width: 28px;
+      height: 28px;
+      background-color: rgba(85, 135, 240, 0.1) !important;
+      border-radius: 3px;
+    }
+   .el-pagination__jump{
+      margin-left: 60px;
+    }
+    .btn-prev,
+    .btn-next {
+      background: none !important;
+    }
+  }
 }
 
 .el-pagination {
-  text-align: center;
-  margin-top: 24px;
   white-space: nowrap;
   color: #303133;
   font-weight: 700;
-  display: flex;
-  justify-content: flex-end;
 }
 </style>
