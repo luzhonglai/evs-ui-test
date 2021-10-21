135cf7ab24cbad1130515de48009555572cbd78b
ZhongLaiLu <32395032@qq.com>
2021-10-21 18:27:40 +0800

    feat： EvsSearchArea 增加配置项

--- packages/EvsSearchArea/index.vue
+++ packages/EvsSearchArea/index.vue
@@ -54,10 +54,13 @@
             >
               <el-input
                 v-model="formData[item.name]"
+                :distance="item.distance || false"
                 style="width: 100%"
                 clearable
                 :placeholder="item.placeholder"
-              ></el-input>
+              >
+                <slot v-if="item.isInputSlot" name="inputSlot"></slot>
+              </el-input>
             </el-form-item>
             <!-- input前面加下拉菜单 -->
             <div
@@ -88,6 +91,7 @@
                 <el-input
                   v-model="formData[item.name]"
                   style="width: 100%"
+                  :distance="item.distance || false"
                   clearable
                   size="mini"
                   :placeholder="item.placeholder"
@@ -148,6 +152,7 @@
                 :start-placeholder="item.startPlaceholder"
                 :end-placeholder="item.endPlaceholder"
                 style="width: 100%"
+                :unlink-panels="item.unlinkPanels || false"
                 :default-time="item.defaultTime"
                 :disabled-date="disabledDate"
               ></el-date-picker>
@@ -184,6 +189,7 @@
                   :range-separator="item.rangeSeparator"
                   :start-placeholder="item.startPlaceholder"
                   :end-placeholder="item.endPlaceholder"
+                  :unlink-panels="item.unlinkPanels || false"
                   style="width: 100%"
                   :default-time="item.defaultTime"
                   :disabled-date="disabledDate"
@@ -231,240 +237,240 @@
   </div>
 </template>
 <script lang="ts">
-  import { defineComponent, computed, reactive, ref } from 'vue'
-  export default defineComponent({
-    name: 'EvsSearchArea',
-    props: {
-      hasFold: {
-        type: Boolean,
-        default: false, // 是否有展开收起功能,默认设置false
-      },
-      rules: Object,
-      formModel: Array, // 需要渲染的form组件集合
-      initData: Object, // 初始化需要显示数据的字段
-      emitName: {
-        type: String,
-        default: '',
-      }, // emit出去的事件名
-      exportModel: Object,
-      isReset: {
-        // 是否显示重置按钮
-        type: Boolean,
-        default: true,
-      },
+import { defineComponent, computed, reactive, ref } from 'vue'
+export default defineComponent({
+  name: 'EvsSearchArea',
+  props: {
+    hasFold: {
+      type: Boolean,
+      default: false, // 是否有展开收起功能,默认设置false
     },
-    setup(props, ctx) {
-      const refFrom: any = ref('')
-      const formData: any = props.initData ? props.initData : reactive({})
-      const defaultTime: any = props.initData ? JSON.parse(JSON.stringify(props.initData)) : reactive({})
+    rules: Object,
+    formModel: Array, // 需要渲染的form组件集合
+    initData: Object, // 初始化需要显示数据的字段
+    emitName: {
+      type: String,
+      default: '',
+    }, // emit出去的事件名
+    exportModel: Object,
+    isReset: {
+      // 是否显示重置按钮
+      type: Boolean,
+      default: true,
+    },
+  },
+  setup(props, ctx) {
+    const refFrom: any = ref('')
+    const formData: any = props.initData ? props.initData : reactive({})
+    const defaultTime: any = props.initData ? JSON.parse(JSON.stringify(props.initData)) : reactive({})
 
-      const selectOptions = computed(() => {
-        return function(item: any) {
-          if (item.defaultProps) {
-            item.options.forEach((option: any) => {
-              option.label = option[item.defaultProps.label]
-              option.value = option[item.defaultProps.value]
-            })
-          }
-          return item.options
+    const selectOptions = computed(() => {
+      return function(item: any) {
+        if (item.defaultProps) {
+          item.options.forEach((option: any) => {
+            option.label = option[item.defaultProps.label]
+            option.value = option[item.defaultProps.value]
+          })
+        }
+        return item.options
+      }
+    })
+    const switchFlag = ref(false) // 展开收起开关
+    // 省市切换清空站列表
+    const changeCity = () => {
+      ctx.emit('changeCity', formData)
+    }
+
+    const handleToggleDateType = (key: any) => {
+      //组件前select切换,清空后面时分秒
+      formData[key] = ''
+    }
+    // 点击查询按钮
+    const submitForm = () => {
+      refFrom.value.validate(async (result: boolean) => {
+        if (result) {
+          const emitName: string = props.emitName as string
+          ctx.emit(emitName ? emitName : 'search', formData)
         }
       })
-      const switchFlag = ref(false) // 展开收起开关
-      // 省市切换清空站列表
-      const changeCity = () => {
-        ctx.emit('changeCity', formData)
-      }
-
-      const handleToggleDateType = (key: any) => {
-        //组件前select切换,清空后面时分秒
-        formData[key] = ''
-      }
-      // 点击查询按钮
-      const submitForm = () => {
-        refFrom.value.validate(async (result: boolean) => {
-          if (result) {
-            const emitName: string = props.emitName as string
-            ctx.emit(emitName ? emitName : 'search', formData)
-          }
-        })
-      }
-      const disabledDate = (time: any): any => {
-        return Date.now() <= time.getTime()
-      }
-      // 重置
-      const resetForm = () => {
-        for (const key in formData) {
-          if (defaultTime && Object.keys(defaultTime).indexOf(key) > -1) {
-            if (Array.isArray(defaultTime[key])) {
-              formData[key] = defaultTime[key]
-            } else {
-              formData[key] = defaultTime[key]
-            }
+    }
+    const disabledDate = (time: any): any => {
+      return Date.now() <= time.getTime()
+    }
+    // 重置
+    const resetForm = () => {
+      for (const key in formData) {
+        if (defaultTime && Object.keys(defaultTime).indexOf(key) > -1) {
+          if (Array.isArray(defaultTime[key])) {
+            formData[key] = defaultTime[key]
           } else {
-            formData[key] = ''
+            formData[key] = defaultTime[key]
           }
+        } else {
+          formData[key] = ''
         }
-        const emitName = props.emitName as string
-        ctx.emit(emitName ? emitName : 'resetForm', formData)
       }
-      return {
-        refFrom,
-        formData,
-        selectOptions,
-        disabledDate,
-        switchFlag,
-        changeCity,
-        handleToggleDateType,
-        resetForm,
-        submitForm,
-      }
-    },
-  })
+      const emitName = props.emitName as string
+      ctx.emit(emitName ? emitName : 'resetForm', formData)
+    }
+    return {
+      refFrom,
+      formData,
+      selectOptions,
+      disabledDate,
+      switchFlag,
+      changeCity,
+      handleToggleDateType,
+      resetForm,
+      submitForm,
+    }
+  },
+})
 </script>
 <style lang="less">
-  .searchCon {
-    .searchArea {
-      .el-input__inner,
-      .el-input_item_inner,
-      .el-input--mini .el-input_item_inner,
-      .el-input--small .el-input_item_inner {
-        width: 100%;
-        border: none;
-        height: 32px;
-        line-height: 32px;
-      }
-    }
-
-    .el-cascader {
-      width: 100% !important;
-    }
-    .el-button {
-      &:nth-child(1),
-      &:nth-child(2) {
-        margin-top: 0 !important;
-      }
-    }
-    .el-select--mini {
+.searchCon {
+  .searchArea {
+    .el-input__inner,
+    .el-input_item_inner,
+    .el-input--mini .el-input_item_inner,
+    .el-input--small .el-input_item_inner {
       width: 100%;
-    }
-    .group {
-      .el-select--mini {
-        width: 120px;
-        margin-left: 0;
-      }
-      .el-input__inner {
-        padding-left: 12px;
-      }
+      border: none;
+      height: 32px;
+      line-height: 32px;
     }
   }
+
+  .el-cascader {
+    width: 100% !important;
+  }
+  .el-button {
+    &:nth-child(1),
+    &:nth-child(2) {
+      margin-top: 0 !important;
+    }
+  }
+  .el-select--mini {
+    width: 100%;
+  }
+  .group {
+    .el-select--mini {
+      width: 120px;
+      margin-left: 0;
+    }
+    .el-input__inner {
+      padding-left: 12px;
+    }
+  }
+}
 </style>
 <style lang="less" scoped>
-  .searchCon {
-    display: flex;
-    // padding: 24px 24px 0;
-    padding-top: 24px;
-    background-color: #fff;
-    .searchArea {
-      flex: 1;
-      ::v-deep(.el-input) {
-        width: 100%;
-      }
-      .search-item {
-        height: 32px;
-        border-radius: 2px;
-        border: 1px solid #dcdcdc;
-        display: flex;
-        flex-direction: row;
-        // align-items: center;
-        margin-top: 16px;
-        margin-bottom: 0px;
-        .label_name {
-          line-height: 1;
-          padding: 0 12px;
-          color: #666;
-          font-size: 12px;
-          position: relative;
-          &::after {
-            position: absolute;
-            top: -1px;
-            right: 0;
-            display: block;
-            content: '|';
-            color: #dcdcdc;
-            height: 32px;
-            line-height: 32px;
-          }
-        }
-        .group {
-          padding: 0;
-        }
-        .label_ {
-          padding-right: 25px;
-        }
-        .input_item {
-          flex: 1;
-        }
-        :deep(label) {
-          min-width: 70px;
-          position: relative;
-          text-align: left;
-          padding-left: 12px;
-          box-sizing: border-box;
-          font-size: 12px;
-          &::after {
-            position: absolute;
-            top: -1px;
-            right: 0;
-            display: block;
-            content: '|';
-            color: #dcdcdc;
-            height: 32px;
-            line-height: 32px;
-          }
-          &::before {
-            position: absolute;
-            left: 4px;
-          }
-        }
-        :deep(.el-form-item__error) {
-          padding-top: 2px;
-        }
-        :deep(.el-form-item__content) {
-          margin-left: 0 !important;
-          flex: 1;
-        }
-      }
-      .el-col {
-        &:nth-child(1),
-        &:nth-child(2),
-        &:nth-child(3) {
-          .search-item {
-            margin-top: 0;
-          }
-        }
-      }
+.searchCon {
+  display: flex;
+  // padding: 24px 24px 0;
+  padding-top: 24px;
+  background-color: #fff;
+  .searchArea {
+    flex: 1;
+    ::v-deep(.el-input) {
+      width: 100%;
     }
-  }
-  .fold {
-    height: 36px;
-    overflow: hidden;
-  }
-  .operator {
-    width: 220px;
-    padding-left: 12px;
-    .operator_l {
-      float: left;
-    }
-    .operator_r {
-      width: 170px;
-      float: right;
+    .search-item {
+      height: 32px;
+      border-radius: 2px;
+      border: 1px solid #dcdcdc;
       display: flex;
-      justify-content: space-between;
-      flex-flow: row wrap;
-      :deep(button) {
-        margin-left: 10px !important;
-        margin-bottom: 12px !important;
+      flex-direction: row;
+      // align-items: center;
+      margin-top: 16px;
+      margin-bottom: 0px;
+      .label_name {
+        line-height: 1;
+        padding: 0 12px;
+        color: #666;
+        font-size: 12px;
+        position: relative;
+        &::after {
+          position: absolute;
+          top: -1px;
+          right: 0;
+          display: block;
+          content: '|';
+          color: #dcdcdc;
+          height: 32px;
+          line-height: 32px;
+        }
+      }
+      .group {
+        padding: 0;
+      }
+      .label_ {
+        padding-right: 25px;
+      }
+      .input_item {
+        flex: 1;
+      }
+      :deep(label) {
+        min-width: 70px;
+        position: relative;
+        text-align: left;
+        padding-left: 12px;
+        box-sizing: border-box;
+        font-size: 12px;
+        &::after {
+          position: absolute;
+          top: -1px;
+          right: 0;
+          display: block;
+          content: '|';
+          color: #dcdcdc;
+          height: 32px;
+          line-height: 32px;
+        }
+        &::before {
+          position: absolute;
+          left: 4px;
+        }
+      }
+      :deep(.el-form-item__error) {
+        padding-top: 2px;
+      }
+      :deep(.el-form-item__content) {
+        margin-left: 0 !important;
+        flex: 1;
+      }
+    }
+    .el-col {
+      &:nth-child(1),
+      &:nth-child(2),
+      &:nth-child(3) {
+        .search-item {
+          margin-top: 0;
+        }
       }
     }
   }
+}
+.fold {
+  height: 36px;
+  overflow: hidden;
+}
+.operator {
+  width: 220px;
+  padding-left: 12px;
+  .operator_l {
+    float: left;
+  }
+  .operator_r {
+    width: 170px;
+    float: right;
+    display: flex;
+    justify-content: space-between;
+    flex-flow: row wrap;
+    :deep(button) {
+      margin-left: 10px !important;
+      margin-bottom: 12px !important;
+    }
+  }
+}
 </style>
